19ca9ca803d1b9f65e13484b1481d112b2d763ce
kmazi <ugochukwu.mazi@andela.com>
2018-06-17 20:02:12 +0100

    chore(webpack): Bundle js files 	- Bundle scss files in js entry file

--- portfolio/static/portfolio/js/entry.js
+++ portfolio/static/portfolio/js/entry.js
@@ -1,41 +1,10 @@
 import "foundation-sites/js/entries/foundation";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel";
 import "../css/custom.scss";
 
 $(document).foundation();
-$('#test-slick').slick({
-  infinity: true,
-  dots: true,
-  speed: 900,
-  autoplay: true,
-});
 
-const viewportHeight = window.innerHeight;
-const headerHeight = $('#header').height();
-const imageHeight = viewportHeight - headerHeight - 26;
-$('.call-out, .shade-layer').height(imageHeight);
-const calloutObj = $('.shade-layer > div');
-const calloutPadding = imageHeight/2 - calloutObj.height()/2;
-calloutObj.css('padding-top', calloutPadding);
-
-//execute at startup
-$(() => {
-  setNavigation();
-});
-
-//A function that sets navigation color
-const setNavigation = () => {
-  let path = window.location.pathname;
-  path = path.replace(/\/$/, "");
-  path = decodeURIComponent(path);
-  
-  $("#header-menu a").each((index, element) => {
-    var href = $(element).attr("href");
-    if (path == href) {
-      $("#header-menu li").removeClass("is-active");
-      $(element).closest('li').addClass("is-active");
-    }
-  });
-};
+var viewportHeight = window.innerHeight;
+var headerHeight = $('#header').height();
+var imageHeight = viewportHeight - headerHeight - 26;
+$('.callout.primary').height(imageHeight);
+$('.shade-layer').height(imageHeight - 224);
